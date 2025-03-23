93c7bbbaaf86e8fb45581fe704d6daff110a049f
wasif kareem <was_if__@192.168.1.5>
2025-03-23 15:29:13 +0530

    :fix:center aligning

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import Notification from './components/Notification'
-import Hero from './components/Hero'
-import Logogallery from './components/Logogallery'
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Notification from './components/Notification';
+import Hero from './components/Hero';
+import Logogallery from './components/Logogallery';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    <div className=' px-5 xl:px-52 pt-3'>
-    <Navbar/>
-    <Notification/>
+    <div className="flex justify-center">
+      <div className="max-w-[1750px]">
+        <div className="px-5 xl:px-52 pt-3">
+          <Navbar />
+          <Notification />
+        </div>
+        <div className="px-5 pt-3 md:px-14 xl:px-60">
+          <Hero />
+          <Logogallery />
+        </div>
+      </div>
     </div>
-    <div className=' px-5 pt-3  md:px-14 xl:px-60'>
-
-    
-     <Hero/>
-     <Logogallery/>
-    </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
